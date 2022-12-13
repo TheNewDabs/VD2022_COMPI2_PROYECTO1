6182972ada2c70abcaee98a210e28744bfd25302
Diego Alejandro Martinez Garcia <36651869+Diamg502@users.noreply.github.com>
2022-12-12 23:04:32 -0600

    Grammar actualizada

--- grammar/grammar.py
+++ grammar/grammar.py
@@ -1,9 +1,62 @@
 import ply.yacc as yacc
 import ply.lex as lex
 
+from sym.Environment import *
+from abstract.Return import *
 
+from instruction.conditionals.If import *
+
+from instruction.functions.Param import *
+from instruction.functions.Function import *
+from instruction.functions.ReturnST import *
+
+from instruction.loops.Break import *
+from instruction.loops.Continue import *
+from instruction.loops.While import *
+from instruction.loops.For import *
+
+from instruction.nativas.Print import *
+from instruction.nativas.ToUpper import *
+from instruction.nativas.ToLower import *
+from instruction.nativas.Length import *
+from instruction.nativas.Trunc import *
+
+from instruction.structs.AssignAccess import *
+from instruction.structs.CreateStruct import *
+from instruction.structs.DeclareStruct import *
+from instruction.structs.StructAttr import *
+
+from instruction.variables.Declaration import *
+
+from instruction.Statement import *
+
+from expressions.Access_array import *
+from expressions.Change_array import *
+from expressions.Access_struct import *
+from expressions.Access import *
+from expressions.Arithmetic import *
+from expressions.Call_func import *
+from expressions.Literal import *
+from expressions.Logical import *
+from expressions.Relational import *
 
 reservadas = {
+    "None": "NOTHING",
+    "int": "INT",
+    "float": "FLOAT",
+    "bool": "BOOL",
+    "string": "STRING",
+    "list": "ARRAY",
+    "Struct": "STRUCT",
+    "or": "OR",
+    "and": "AND",
+    "not": "NOT",
+    "uppercase": "UPPERCASE",
+    "lowercase": "LOWERCASE",
+
+
+
+    #---------------
     "println": "PRINTLN",
     "print": "PRINT",
     "while": "WHILE",
@@ -12,39 +65,32 @@ reservadas = {
     "in": "IN",
     "elseif": "ELSEIF",
     "else": "ELSE",
-    "nothing": "NOTHING",
-    "Int64": "INT",
-    "Float64": "FLOAT",
-    "Array": "ARRAY",
-    "Bool": "BOOL",
+
+    
     "Char": "CHAR",
-    "String": "STRING",
-    "struct": "STRUCT",
-    "uppercase": "UPPERCASE",
-    "lowercase": "LOWERCASE",
+
     "log10": "LOG10",
     "log": "LOG",
     "sin": "SIN",
     "cos": "COS",
     "tan": "TAN",
     "trunc": "TRUNC",
-    "float": "TOFLOAT",
-    "string": "TOSTRING",
+    "foat2": "TOFLOAT",
+    "sting2": "TOSTRING",
     "parse": "PARSE",
     "typeof": "TYPEOF",
     "sqrt": "SQRT",
     "length": "LENGTH",
     "push": "PUSH",
     "pop": "POP",
-    "function": "FUNCTION",
+    "def": "FUNCTION",
     "break": "BREAK",
     "continue": "CONTINUE",
     "return": "RETURN",
     "end": "END",
     "true": "TRUE",
     "false": "FALSE",
-    "local": "LOCAL",
-    "global": "GLOBAL"
+    "local": "LOCAL"
 }
 
 tokens = [
@@ -67,16 +113,14 @@ tokens = [
     "MENIGUALQUE",
     "MAYQUE",
     "MAYIGUALQUE",
-    "OR",
-    "AND",
-    "NOT",
     "IGUALQUE",
     "NIGUALQUE",
     "DECIMAL",
     "ENTERO",
     "CADENA",
     "CARACTER",
-    "ID"
+    "ID",
+    "newline"
 ] + list(reservadas.values())
 
 # Tokens
@@ -98,9 +142,6 @@ t_MENQUE = r'<'
 t_MENIGUALQUE = r'<='
 t_MAYQUE = r'>'
 t_MAYIGUALQUE = r'>='
-t_OR = r'\|\|'
-t_AND = r'&&'
-t_NOT = r'!'
 t_IGUALQUE = r'=='
 t_NIGUALQUE = r'!='
 t_IGUAL = r'='
@@ -132,6 +173,11 @@ def t_ID(t):
     t.type = reservadas.get(t.value, 'ID')
     return t
 
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    t.value = '\n'
+    return t
 
 def t_CADENA(t):
     r'\".*?\"'
@@ -165,16 +211,11 @@ def t_COMENTARIO_SIMPLE(t):
 t_ignore = " \t"
 
 
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += t.value.count("\n")
-
-
 def t_error(t):
     error_lexico = {}
     error_lexico["type"] = "lexico"
     error_lexico["value"] = "Caracter no reconocido " + t.value[0] + "\n"
-    
+    Environment.errores.append(error_lexico)
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
@@ -206,29 +247,33 @@ def p_init(t):
 def p_instrucciones_lista(t):
     '''instrucciones    : instrucciones instruccion
                         | instruccion'''
-    
+    if len(t) != 2:
+        t[1].append(t[2])
+        t[0] = t[1]
+    else:
+        t[0] = [t[1]]
 
 
 def p_instruccion(t):
-    '''instruccion      : print_instr PTCOMA
-                        | println_instr PTCOMA
-                        | definicion_instr PTCOMA
-                        | asignacion_instr PTCOMA
-                        | asignacion_arreglo_instr PTCOMA
-                        | definicion_asignacion_instr PTCOMA
-                        | call_function PTCOMA
-                        | declare_function PTCOMA
-                        | return_state PTCOMA
-                        | break_state PTCOMA
-                        | continue_state PTCOMA
-                        | if_state PTCOMA
-                        | while_state PTCOMA
-                        | for_state PTCOMA
-                        | nativas PTCOMA
-                        | create_struct PTCOMA
-                        | declare_struct PTCOMA
-                        | assign_access PTCOMA'''
-    
+    '''instruccion      : print_instr newline
+                        | println_instr newline
+                        | definicion_instr newline
+                        | asignacion_instr newline
+                        | asignacion_arreglo_instr newline
+                        | definicion_asignacion_instr newline
+                        | call_function newline
+                        | declare_function newline
+                        | return_state newline
+                        | break_state newline
+                        | continue_state newline
+                        | if_state newline
+                        | while_state newline
+                        | for_state newline
+                        | nativas newline
+                        | create_struct newline
+                        | declare_struct newline
+                        | assign_access newline'''
+    t[0] = t[1]
 
 
 def p_expression(t):
@@ -249,7 +294,59 @@ def p_expression(t):
                         | expression OR expression
                         | expression AND expression
                         | final_expression'''
-    
+    if len(t) == 2:
+        t[0] = t[1]
+    elif len(t) == 3:
+        if t[1] == "-":
+            t[0] = Arithmetic(Literal(0, Type.INT, t.lineno(1), t.lexpos(
+                0)), t[2], ArithmethicOption.MINUS, t.lineno(1), t.lexpos(0))
+        else:
+            t[0] = Logical(t[2], Literal(True, Type.BOOL, t.lineno(
+                1), t.lexpos(0)), LogicOption.NOT, t.lineno(1), t.lexpos(0))
+    else:
+        if t[2] == "+":
+            t[0] = Arithmetic(t[1], t[3], ArithmethicOption.PLUS,
+                              t.lineno(1), t.lexpos(0))
+        elif t[2] == "-":
+            t[0] = Arithmetic(
+                t[1], t[3], ArithmethicOption.MINUS, t.lineno(1), t.lexpos(0))
+        elif t[2] == "*":
+            t[0] = Arithmetic(
+                t[1], t[3], ArithmethicOption.TIMES, t.lineno(1), t.lexpos(0))
+        elif t[2] == "/":
+            t[0] = Arithmetic(t[1], t[3], ArithmethicOption.DIV,
+                              t.lineno(1), t.lexpos(0))
+        elif t[2] == "^":
+            t[0] = Arithmetic(t[1], t[3], ArithmethicOption.RAISED,
+                              t.lineno(1), t.lexpos(0))
+        elif t[2] == "%":
+            t[0] = Arithmetic(
+                t[1], t[3], ArithmethicOption.MODULE, t.lineno(1), t.lexpos(0))
+        elif t[2] == "or":
+            t[0] = Logical(t[1], t[3], LogicOption.OR,
+                           t.lineno(1), t.lexpos(0))
+        elif t[2] == "and":
+            t[0] = Logical(t[1], t[3], LogicOption.AND,
+                           t.lineno(1), t.lexpos(0))
+        elif t[2] == "<":
+            t[0] = Relational(t[1], t[3], RelationalOption.LESS,
+                              t.lineno(1), t.lexpos(0))
+        elif t[2] == ">":
+            t[0] = Relational(
+                t[1], t[3], RelationalOption.GREATER, t.lineno(1), t.lexpos(0))
+        elif t[2] == "<=":
+            t[0] = Relational(
+                t[1], t[3], RelationalOption.LESSEQUAL, t.lineno(1), t.lexpos(0))
+        elif t[2] == ">=":
+            t[0] = Relational(
+                t[1], t[3], RelationalOption.GREATEREQUAL, t.lineno(1), t.lexpos(0))
+        elif t[2] == "==":
+            t[0] = Relational(t[1], t[3], RelationalOption.EQUAL,
+                              t.lineno(1), t.lexpos(0))
+        elif t[2] == "!=":
+            t[0] = Relational(
+                t[1], t[3], RelationalOption.DISTINCT, t.lineno(1), t.lexpos(0))
+
 
 # todo falta potencia y modulo
 def p_final_expression(t):
@@ -266,7 +363,34 @@ def p_final_expression(t):
                             | call_function
                             | access_struct
                             | nativas'''
-    
+    if len(t) == 2:
+        if t.slice[1].type == "ENTERO":
+            t[0] = Literal(t[1], Type.INT, t.lineno(1), t.lexpos(0))
+        if t.slice[1].type == "DECIMAL":
+            t[0] = Literal(t[1], Type.FLOAT, t.lineno(1), t.lexpos(0))
+        elif t.slice[1].type == "FALSE":
+            t[0] = Literal(False, Type.BOOL, t.lineno(1), t.lexpos(0))
+        elif t.slice[1].type == "TRUE":
+            t[0] = Literal(True, Type.BOOL, t.lineno(1), t.lexpos(0))
+        elif t.slice[1].type == "CADENA":
+            t[0] = Literal(str(t[1]), Type.STRING, t.lineno(1), t.lexpos(0))
+        elif t.slice[1].type == "CARACTER":
+            t[0] = Literal(str(t[1]), Type.CHAR, t.lineno(1), t.lexpos(0))
+        elif t.slice[1].type == "ID":
+            t[0] = Access(t[1], t.lineno(1), t.lexpos(0))
+        elif t.slice[1].type == "call_function" or t.slice[1].type == "access_struct":
+            t[0] = t[1]
+        elif t.slice[1].type == "nativas":
+            t[0] = t[1]
+    elif len(t) == 3:
+        t[0] = AccessArray(t[1], t[2], t.lineno(1), t.lexpos(0))
+    else:
+        if t.slice[1].type == "PARIZQ":
+            t[0] = t[2]
+        else:
+            t[0] = Literal(t[2], Type.ARRAY, t.lineno(1), t.lexpos(0))
+        # TODO tengo que ver lo de los corchetes
+
 
 def p_nativas(t):
     '''nativas          : LOG PARIZQ ENTERO COMA expression PARDER
@@ -287,16 +411,51 @@ def p_nativas(t):
                         | PUSH PARIZQ expression COMA expression PARDER
                         | POP PARIZQ expression PARDER
                         '''
-    
+    if t.slice[1].type == "LOG":
+        pass
+    elif t.slice[1].type == "LOG10":
+        pass
+    elif t.slice[1].type == "COS":
+        pass
+    elif t.slice[1].type == "SIN":
+        pass
+    elif t.slice[1].type == "TAN":
+        pass
+    elif t.slice[1].type == "SQRT":
+        pass
+    elif t.slice[1].type == "UPPERCASE":
+        t[0] = ToUpper(t[3], t.lineno(1), t.lexpos(0))
+    elif t.slice[1].type == "LOWERCASE":
+        t[0] = ToLower(t[3], t.lineno(1), t.lexpos(0))
+    elif t.slice[1].type == "TOSTRING":
+        pass
+    elif t.slice[1].type == "TOFLOAT":
+        pass
+    elif t.slice[1].type == "TRUNC":
+        if len(t) == 5:
+            t[0] = Trunc(t.lineno(1), t.lexpos(0), t[3])
+        else:
+            t[0] = Trunc(t.lineno(1), t.lexpos(0), t[5])
+    elif t.slice[1].type == "TYPEOF":
+        pass
+    elif t.slice[1].type == "PARSE":
+        pass
+    elif t.slice[1].type == "LENGTH":
+        t[0] = Length(t.lineno(1), t.lexpos(0), t[3])
+    elif t.slice[1].type == "PUSH":
+        pass
+    elif t.slice[1].type == "POP":
+        pass
+
 
 def p_print_instr(t):
     'print_instr    : PRINT PARIZQ exp_list PARDER'
-    
+    t[0] = Print(t[3], t.lineno(1), t.lexpos(0), False)
 
 
 def p_println_instr(t):
     'println_instr  : PRINTLN PARIZQ exp_list PARDER'
-   
+    t[0] = Print(t[3], t.lineno(1), t.lexpos(0), True)
 
 
 def p_tipo(t):
@@ -308,31 +467,51 @@ def p_tipo(t):
                 | ARRAY
                 | NOTHING
     '''
-    
+    if t.slice[1].type == "INT":
+        t[0] = Type.INT
+    elif t.slice[1].type == "FLOAT":
+        t[0] = Type.FLOAT
+    elif t.slice[1].type == "BOOL":
+        t[0] = Type.BOOL
+    elif t.slice[1].type == "CHAR":
+        t[0] = Type.CHAR
+    elif t.slice[1].type == "STRING":
+        t[0] = Type.STRING
+    elif t.slice[1].type == "NOTHING":
+        t[0] = Type.NULL
+    elif t.slice[1].type == "ARRAY":
+        t[0] = Type.ARRAY
 
 
 def p_definicion_instr(t):
-    '''definicion_instr   :  LOCAL ID
-                            | GLOBAL ID'''
-    
+    '''definicion_instr   :  LOCAL ID'''
+    if t.slice[1].type == "LOCAL":
+        t[0] = Declaration(t[2], 0, t.lineno(1), t.lexpos(0))
 
 
 def p_asignacion_instr(t):
     '''asignacion_instr     : ID IGUAL expression
-                            | LOCAL ID IGUAL expression
-                            | GLOBAL ID IGUAL expression'''
-    
+                            | LOCAL ID IGUAL expression'''
+    if len(t) == 4:
+        t[0] = Declaration(t[1], t[3], t.lineno(1), t.lexpos(0))
+    else:
+        if t.slice[1].type == "LOCAL":
+            t[0] = Declaration(t[2], t[4], t.lineno(1), t.lexpos(0))
 
 
 def p_definicion_asignacion_instr(t):
-    '''definicion_asignacion_instr  : ID IGUAL expression DOSP DOSP tipo
-                                    | LOCAL ID IGUAL expression DOSP DOSP tipo
-                                    | GLOBAL ID IGUAL expression DOSP DOSP tipo'''
-    
+    '''definicion_asignacion_instr  : ID IGUAL expression DOSP tipo
+                                    | LOCAL ID IGUAL expression DOSP tipo'''
+    if len(t) == 6:
+        t[0] = Declaration(t[1], t[3], t.lineno(1), t.lexpos(0))
+    else:
+        if t.slice[1].type == "LOCAL":
+            t[0] = Declaration(t[2], t[4], t.lineno(1), t.lexpos(0))
+
 
 def p_asignacion_arreglo_instr(t):
     '''asignacion_arreglo_instr     : ID index_list IGUAL expression'''
-    
+    t[0] = ChangeArray(t[1], t[2], t[4], t.lineno(1), t.lexpos(0))
 
 
 # todo tengo que hacer que se asignar y declarar los arreglos con index
@@ -340,32 +519,45 @@ def p_asignacion_arreglo_instr(t):
 def p_call_function_instr(t):
     '''call_function    : ID PARIZQ PARDER
                         | ID PARIZQ exp_list PARDER'''
-    
+    if len(t) == 4:
+        t[0] = CallFunc(t[1], [], t.lineno(1), t.lexpos(0))
+    else:
+        t[0] = CallFunc(t[1], t[3], t.lineno(1), t.lexpos(0))
 
 
 def p_exp_list_instr(t):
     '''exp_list         : exp_list COMA expression
                         | expression'''
-    
+    if len(t) == 2:
+        t[0] = [t[1]]
+    else:
+        t[1].append(t[3])
+        t[0] = t[1]
 
 
 def p_index_list_instr(t):
     '''index_list       : index_list CORCHETEIZQ expression CORCHETEDER
                         | CORCHETEIZQ expression CORCHETEDER'''
-    
+    if len(t) == 4:
+        t[0] = [t[2]]
+    else:
+        t[1].append(t[3])
+        t[0] = t[1]
 
 
 def p_statement(t):
     '''statement        : instrucciones'''
-    
+    t[0] = Statement(t[1], t.lineno(1), t.lexpos(0))
 
 
 def p_declare_function(t):
-    '''declare_function     : FUNCTION ID PARIZQ PARDER DOSP DOSP tipo statement END
-                            | FUNCTION ID PARIZQ dec_params PARDER DOSP DOSP tipo statement END
-                            | FUNCTION ID PARIZQ dec_params PARDER statement END
-                            | FUNCTION ID PARIZQ PARDER statement END'''
-    
+    '''declare_function     : FUNCTION ID PARIZQ dec_params PARDER statement newline
+                            | FUNCTION ID PARIZQ PARDER statement newline'''
+    if len(t) == 8:
+        t[0] = Function(t[2], t[4], Type.NULL, t[6], t.lineno(1), t.lexpos(0))
+    elif len(t) == 7:
+        t[0] = Function(t[2], [], Type.NULL, t[5], t.lineno(1), t.lexpos(0))
+
 
 def p_dec_params(t):
     '''dec_params :    dec_params COMA ID DOSP DOSP tipo
@@ -374,67 +566,121 @@ def p_dec_params(t):
                     | ID DOSP DOSP tipo
                     | ID DOSP DOSP ID
                     | ID'''
-    
+    if len(t) == 5:
+        if t.slice[4].type == 'ID':
+            t[0] = [Param(t[1], Type.STRUCT, t.lineno(1), t.lexpos(0), t[4])]
+        else:
+            t[0] = [Param(t[1], t[4], t.lineno(1), t.lexpos(0))]
+    elif len(t) == 2:
+        t[0] = [Param(t[1], Type.ARRAY, t.lineno(1), t.lexpos(0))]
+    elif len(t) == 4:
+        t[1].append(Param(t[3], Type.ARRAY, t.lineno(1), t.lexpos(0)))
+        t[0] = t[1]
+    else:
+        if t.slice[6].type == "ID":
+            t[1].append(Param(t[3], Type.STRUCT,
+                        t.lineno(1), t.lexpos(0), t[6]))
+            t[0] = t[1]
+        else:
+            t[1].append(Param(t[3], t[6], t.lineno(1), t.lexpos(0)))
+            t[0] = t[1]
 
 
 def p_if_state(t):
     '''if_state     : IF expression statement END
                     | IF expression statement ELSE statement END
                     | IF expression statement else_if_list END'''
-    
+    if len(t) == 5:
+        t[0] = If(t[2], t[3], t.lineno(1), t.lexpos(0))
+    elif len(t) == 6:
+        t[0] = If(t[2], t[3], t.lineno(1), t.lexpos(0), t[4])
+    elif len(t) == 7:
+        t[0] = If(t[2], t[3], t.lineno(1), t.lexpos(0), t[5])
+
 
 def p_else_if_list(t):
     '''else_if_list     : ELSEIF expression statement
                         | ELSEIF expression statement ELSE statement
                         | ELSEIF expression statement else_if_list'''
-    
+    if len(t) == 4:
+        t[0] = If(t[2], t[3], t.lineno(1), t.lexpos(0))
+    elif len(t) == 5:
+        t[0] = If(t[2], t[3], t.lineno(1), t.lexpos(0), t[4])
+    elif len(t) == 6:
+        t[0] = If(t[2], t[3], t.lineno(1), t.lexpos(0), t[5])
+
 
 def p_while_state(t):
     '''while_state      : WHILE expression statement END'''
-    
+    t[0] = While(t[2], t[3], t.lineno(1), t.lexpos(0))
+
 
 def p_for_state(t):
     '''for_state        : FOR ID IN expression DOSP expression statement END
                         | FOR ID IN expression statement END'''
-    
+    if len(t) == 9:
+        t[0] = For(t[2], t[4], t[7], t.lineno(1), t.lexpos(0), t[6])
+    else:
+        t[0] = For(t[2], t[4], t[5], t.lineno(1), t.lexpos(0))
+
 
 def p_break(t):
     '''break_state      : BREAK'''
-    
+    t[0] = Break(t.lineno(1), t.lexpos(0))
+
 
 def p_continue(t):
     '''continue_state      : CONTINUE'''
-    
+    t[0] = Continue(t.lineno(1), t.lexpos(0))
+
 
 def p_return(t):
     '''return_state     : RETURN
                         | RETURN expression'''
-    
+    if len(t) == 2:
+        t[0] = ReturnSt(None, t.lineno(1), t.lexpos(0))
+    else:
+        t[0] = ReturnSt(t[2], t.lineno(1), t.lexpos(0))
+
 
 def p_createStruct(t):
     'create_struct : STRUCT ID attList END'
-    
+    t[0] = CreateStruct(t[2], t[3], t.lineno(1), t.lexpos(0))
+
 
 def p_attList(t):
     '''attList :  attList ID DOSP DOSP tipo PTCOMA
                 | ID DOSP DOSP tipo PTCOMA'''
-    
+    if len(t) == 6:
+        t[0] = [StructAttribute(t[1], t[4], t.lineno(1), t.lexpos(0))]
+    else:
+        t[1].append(StructAttribute(t[2], t[5], t.lineno(1), t.lexpos(0)))
+        t[0] = t[1]
+
 
 def p_declareStruct(t):
     'declare_struct : ID DOSP DOSP ID'
-    
+    t[0] = DeclareStruct(t[1], t[4], t.lineno(1), t.lexpos(0))
+
 
 def p_assignAccess(t):
     'assign_access : ID PUNTO ID IGUAL expression'
+    t[0] = AssignAccess(t[1], t[3], t[5], t.lineno(1), t.lexpos(0))
 
 
 def p_access_struct(t):
     '''access_struct : ID PUNTO ID'''
+    t[0] = AccessStruct(t[1], t[3], t.lineno(1), t.lexpos(0))
 
 
 def p_error(t):
     print(t)
     print("Error sintáctico en '%s'" % t.value)
+    error_sintactico = {}
+    error_sintactico["type"] = "sintactico"
+    error_sintactico["value"] = "Error sintactico en " + t.value+"\n"
+    Environment.errores.append(error_sintactico)
+    Print.printlist += "Error sintactico en " + t.value+"\n"
 
 
 parser = yacc.yacc()
